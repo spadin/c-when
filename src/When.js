835c7ef9233ce5e74cf80f8ec0e9287f01d95c6e
Sandro Padin <sandropadin@gmail.com>
2017-07-30 10:22:06 -0700

    When now accepts a function that is invoked when predicate is true

--- src/When.js
+++ src/When.js
@@ -1,5 +1,9 @@
-const When = ({predicate, children}) => (
-  (predicate)?(children):(null)
-);
+const isFunction = (obj) => (typeof obj === 'function');
+
+const invokeOrReturn = (obj) => (isFunction(obj) ? obj() : obj);
+
+const When = ({predicate, children}) => {
+  return (predicate)?(invokeOrReturn(children)):(null)
+};
 
 export default When;
--- src/When.test.js
+++ src/When.test.js
@@ -2,9 +2,12 @@ import React from 'react';
 import When from './When';
 import chai, {expect} from 'chai';
 import chaiEnzyme from 'chai-enzyme';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
 import {shallow} from 'enzyme';
 
 chai.use(chaiEnzyme());
+chai.use(sinonChai);
 
 describe('<When/>', () => {
   it('renders content when the predicate is true', () => {
@@ -26,5 +29,29 @@ describe('<When/>', () => {
       </When>
     )
     expect(component).to.be.empty;
+  });
+
+  it('invokes the function passed in as the child', () => {
+    const spy = sinon.spy(() => (null));
+
+    shallow(
+      <When predicate={true}>
+        {spy}
+      </When>
+    );
+
+    expect(spy).to.have.been.called;
+  });
+
+  it('invokes the function passed in as the child', () => {
+    const child = <span>Hello World!</span>;
+    const spy = sinon.spy(() => (child));
+    const component = shallow(
+      <When predicate={true}>
+        {spy}
+      </When>
+    );
+
+    expect(component).to.contain(child);
   });
 });
