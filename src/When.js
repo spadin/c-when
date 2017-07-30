ebddd462864aac842075dfc4fdd42f1354f241b0
Sandro Padin <sandropadin@gmail.com>
2017-07-30 10:45:00 -0700

    Small syntax change

--- src/When.js
+++ src/When.js
@@ -2,8 +2,8 @@ const isFunction = (obj) => (typeof obj === 'function');
 
 const invokeOrReturn = (obj) => (isFunction(obj) ? obj() : obj);
 
-const When = ({predicate, children}) => {
-  return (predicate)?(invokeOrReturn(children)):(null)
-};
+const When = ({predicate, children}) => (
+  (predicate)?(invokeOrReturn(children)):(null)
+);
 
 export default When;
